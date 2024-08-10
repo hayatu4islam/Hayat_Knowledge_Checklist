9f10b5b4f08e2032d51e8f835b8b86a8c58cb264
Hayat <hayatu4islam@yahoo.com>
2024-08-10 18:28:48 +0100

    try fixing...

--- server/utils/config.js
+++ server/utils/config.js
@@ -12,14 +12,10 @@ function createDatabaseUrl() {
 		return process.env.DATABASE_URL;
 	}
 	// const host = process.env.DB_HOST ?? "localhost";
-	const host = process.env.DB_HOST ?? "dpg-cqlttulds78s73acbki0-a";
-	const name = process.env.DB_NAME ?? "cyf";
-	const password =
-		process.env.DB_PASS ??
-		process.env.DB_PASSWORD ??
-		"kJHZgijUm4bzleteAfGNMBy2nKK8M1Rg";
+	const host = process.env.DB_HOST ?? "";
+	const name = process.env.DB_NAME ?? "cyf_39kh_0tiq";
+	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "";
 	const port = process.env.DB_PORT ?? "5432";
-	const username =
-		process.env.DB_USER ?? process.env.DB_USERNAME ?? "cyf_39kh_0tiq_user";
+	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "";
 	return `postgresql://${username}:${password}@${host}:${port}/${name}`;
 }
