a459c2a092981dee8a3e3899033009117789c0ef
Hayat <hayatu4islam@yahoo.com>
2024-08-12 15:09:43 +0100

    Finall fix

--- server/utils/config.js
+++ server/utils/config.js
@@ -13,10 +13,7 @@ function createDatabaseUrl() {
 	}
 	const host = process.env.DB_HOST ?? "dpg-cqlttulds78s73acbki0-a";
 	const name = process.env.DB_NAME ?? "cyf_39kh_0tiq";
-	const password =
-		process.env.DB_PASS ??
-		process.env.DB_PASSWORD ??
-		"kJHZgijUm4bzleteAfGNMBy2nKK8M1Rg";
+	const password = process.env.DB_PASS ?? process.env.DB_PASSWORD ?? "kJHZgijUm4bzleteAfGNMBy2nKK8M1Rg";
 	const port = process.env.DB_PORT ?? "5432";
 	const username = process.env.DB_USER ?? process.env.DB_USERNAME ?? "cyf_39kh_0tiq_user";
 	return `postgresql://${username}:${password}@${host}:${port}/${name}`;
